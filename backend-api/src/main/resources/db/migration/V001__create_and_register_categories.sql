0de2805118c25d27dbf11bcacb82fa91827ee88c
Cleiton Fernando Lima Sena <cleiton.sena@7comm.com.br>
2019-07-17 15:20:20 -0300

    FlyWay config and first migration V001

---
+++ backend-api/src/main/resources/db/migration/V001__create_and_register_categories.sql
@@ -0,0 +1,10 @@
+CREATE TABLE category (
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO category vaues ('Lazer');
+INSERT INTO category vaues ('Alimentação');
+INSERT INTO category vaues ('Supermecado');
+INSERT INTO category vaues ('Farmácia');
+INSERT INTO category vaues ('Outros');
